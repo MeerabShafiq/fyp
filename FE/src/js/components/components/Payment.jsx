19ab39e80fdbd9802c01c9513f13f04ef48f13b5
waleed <root2hack@gmail.com>
2023-04-18 00:59:02 -0400

    chnages

--- FE/src/js/components/components/Payment.jsx
+++ FE/src/js/components/components/Payment.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import CheckoutForm from "./PaymentForm";
