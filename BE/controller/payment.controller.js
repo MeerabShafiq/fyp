cd7308c07ebfd356cc9e703631cb6d6ea0828773
Afaq Ahmed <afaq1.02.98@gmail.com>
2023-04-26 03:21:47 +0500

    added

--- BE/controller/payment.controller.js
+++ BE/controller/payment.controller.js
@@ -1,39 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
+
 app.use(cors());
 const request = require('requests')
 
 
 exports.PaymentCheck = async(req,res)=>{
-
-const URL = toString('https://api.eu-de.apiconnect.appdomain.cloud/easypaisaapigw-telenorbankpk-tmbdev/dev-catalog/CashDeposit/CashDeposit')
-    const options = {
-        method: 'POST',
-        URL: URL,
-        headers: {
-          'X-IBM-Client-Id': '88996644-d975-4058-885e-2b34286812a4',
-          'X-IBM-Client-Secret': '1c48aa28-2614-486f-aa2c-cbc86cc7ab51',
-          'content-type': 'application/json',
-          accept: 'application/json'
-        },
-       
+      const headers = {
+        'X-IBM-Client-Id': 'ce1d135d-9b55-4b3f-829b-dc5e924fdf25',
+        'X-IBM-Client-Secret': '1c48aa28-2614-486f-aa2c-cbc86cc7ab51',
+        'X-Channel': 'REPLACE_THIS_VALUE',
+        'X-Hash-Value': 'REPLACE_THIS_VALUE',
+        'content-type': 'application/json',
+        accept: 'application/json'
       };
-  
-      request(options, function (error, response, body) {
-        if (error) 
-        {
-       
-          throw new Error(error);
-        }
-        else{
-          console.log(body);
-        }
-     
-      });
-    
-        
-    
+      
+      const data = {
+        MSISDN: 'tovj'
+      };
+      
+      axios.post('https://api.eu-de.apiconnect.appdomain.cloud/easypaisaapigw-telenorbankpk-tmbdev/dev-catalog/CashDeposit/CustomerTitleFetch', data, { headers })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      
 }
 
 
